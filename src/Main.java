6a49de1fc0c1bda3f468fc2a4abf2ef0ed96a6b0
Ian MacLeod <imac@Ians-MBP.fios-router.home>
2020-12-05 13:59:19 -0500

    Created a verifier class and instantiated it

--- src/Main.java
+++ src/Main.java
@@ -4,46 +4,16 @@ public class Main {
 	public static void main(String[] args) {
 		
 		String isbn = "9780716703440";
-		int sum = 0;
-		if(isbn.length() == 10) {
-			int position = 10;
-			for(int i = 0; i < 10; i++) {
-				sum = sum + (Integer.parseInt(String.valueOf(isbn.charAt(i))) * position);
-				position--;
-			}
-			
-			if(sum % 11 == 0) {
-				System.out.println("valid");
-			}else {
-				System.out.println("invalid");
-			}
+		
+		Verifier verifier = new Verifier();
+		boolean isValid = verifier.verify(isbn);
+		
+		if(isValid == true) {
+			System.out.println(isbn + " is a valid isbn number!");
+		}else {
+			System.out.println(isbn + " is not a valid isbn number!");
 		}
 		
-		if(isbn.length() == 13) {
-			//int position = 13;
-			int index = 1;
-			for(int i = 0; i < 13; i++) {
-				if(index % 2 == 0) {
-					sum = sum + (Integer.parseInt(String.valueOf(isbn.charAt(i))) * 3);
-				}else {
-					sum = sum + (Integer.parseInt(String.valueOf(isbn.charAt(i))) * 1);
-				}
-				index++;
-				
-			}
-			
-			if(sum % 10 == 0) {
-				System.out.println("valid");
-			}else {
-				System.out.println("invalid");
-			}
-		}
-		
-		
-		
-		
-		
-
 	}
 
 }
--- src/Verifier.java
+++ src/Verifier.java
@@ -3,5 +3,45 @@ public class Verifier {
 	
 	private String isbn;
 	
+	public boolean verify(String isbn) {
+		boolean answer;
+		int sum = 0;
+		if(isbn.length() == 10) {
+			int position = 10;
+			for(int i = 0; i < 10; i++) {
+				sum = sum + (Integer.parseInt(String.valueOf(isbn.charAt(i))) * position);
+				position--;
+			}
+			
+			if(sum % 11 == 0) {
+				answer = true;
+			}else {
+				answer = false;
+			}
+		}else if(isbn.length() == 13) {
+			int index = 1;
+			for(int i = 0; i < 13; i++) {
+				if(index % 2 == 0) {
+					sum = sum + (Integer.parseInt(String.valueOf(isbn.charAt(i))) * 3);
+				}else {
+					sum = sum + (Integer.parseInt(String.valueOf(isbn.charAt(i))) * 1);
+				}
+				index++;
+				
+			}
+			
+			if(sum % 10 == 0) {
+				answer = true;
+			}else {
+				answer = false;
+			}
+		}else {
+			return false;
+		}
+		
+		return answer;
+		
+		
+	}
 
 }
