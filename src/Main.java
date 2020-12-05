dcfeb80e7d4c91fa36779cc8439e9ed65ac777f7
Ian MacLeod <imac@Ians-MBP.fios-router.home>
2020-12-05 13:45:48 -0500

    Added code for 10 digit verification

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,25 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String isbn = "0716703440";
+		int sum = 0;
+		if(isbn.length() == 10) {
+			int position = 10;
+			for(int i = 0; i < 10; i++) {
+				sum = sum + (Integer.parseInt(String.valueOf(isbn.charAt(i))) * position);
+				position--;
+			}
+		}
+		
+		if(sum % 11 == 0) {
+			System.out.println("valid");
+		}else {
+			System.out.println("invalid");
+		}
+		
+		
+		
 
 	}
 
--- src/Verifier.java
+++ src/Verifier.java
@@ -1,4 +1,7 @@
 
 public class Verifier {
+	
+	private String isbn;
+	
 
 }
